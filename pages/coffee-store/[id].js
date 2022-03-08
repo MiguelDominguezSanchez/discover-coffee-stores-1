ef338e349fe6ed75741807353e1df5863604a632
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-08 20:29:18 +0100

    021 Coffee Store Page Styling (continued)

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import cls from 'classnames'
+
 import coffeeStoreData from '../../data/coffee-stores.json'
 
 import styles from '../../styles/coffee-store.module.css'
@@ -42,6 +44,10 @@ const CoffeeStore = (props) => {
 
 	const { address, name, neighbourhood, imgUrl } = props.coffeeStore
 
+	const handleUpvoteButton = () => {
+		console.log('handle upvote')
+	}
+
 	return (
 		<div className={styles.layout}>
 			<Head>{name}</Head>
@@ -63,9 +69,33 @@ const CoffeeStore = (props) => {
 						alt={name}
 					></Image>
 				</div>
-				<div className={styles.col2}>
-					<p>{address}</p>
-					<p>{neighbourhood}</p>
+				<div className={cls('glass', styles.col2)}>
+					<div className={styles.iconWrapper}>
+						<Image
+							src='/static/icons/places.svg'
+							width='24'
+							height='24'
+							alt=''
+						/>
+						<p className={styles.text}>{address}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image
+							src='/static/icons/nearMe.svg'
+							width='24'
+							height='24'
+							alt=''
+						/>
+						<p className={styles.text}>{neighbourhood}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src='/static/icons/star.svg' width='24' height='24' alt='' />
+						<p className={styles.text}>1</p>
+					</div>
+
+					<button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+						Up vote
+					</button>
 				</div>
 			</div>
 		</div>
