c242793d9dfcbab781a60728d9f65f39b91afd2c
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-08 17:23:26 +0100

    010 Implementiing Static Site Generation (SSG)

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,16 @@ import Card from '../components/card'
 
 import coffeeStores from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps(context) {
+	return {
+		props: {
+			coffeeStores,
+		}, // will be passed to the page component as props
+	}
+}
+
+export default function Home(props) {
+	console.log(props)
 	const handleOnBannerBtnClick = () => {
 		console.log('hi banner button')
 	}
@@ -37,6 +46,7 @@ export default function Home() {
 					{coffeeStores.map((coffeeStore) => {
 						return (
 							<Card
+								key={coffeeStore.id}
 								name={coffeeStore.name}
 								imgUrl={coffeeStore.imgUrl}
 								href={`/coffee-store/${coffeeStore.id}`}
